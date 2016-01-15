d3d8c8b3d5ef668fe6fa2a65b666f8e163c29ff9
Ice3ider <ice3ider@gmail.com>
2016-01-15 13:14:04 +0100

    Resolved #58

--- src/main/java/org/ameba/aop/ServiceLayerAspect.java
+++ src/main/java/org/ameba/aop/ServiceLayerAspect.java
@@ -79,10 +79,15 @@ public class ServiceLayerAspect  {
      * @param ex The root exception that is thrown
      * @return Returns the exception to be thrown
      */
-    public Exception translateException(Throwable ex) {
+    public Exception translateException(Exception ex) {
         if (EXC_LOGGER.isErrorEnabled()) {
             EXC_LOGGER.error("[S] Service Layer Exception: " + ex.getLocalizedMessage(), ex);
         }
+
+        if (ex instanceof AbstractBehaviorAwareException) {
+            return ex;
+        }
+
         Optional<Exception> handledException = doTranslateException(ex);
         if (handledException.isPresent()) {
             return handledException.get();
