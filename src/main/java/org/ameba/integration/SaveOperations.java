6267e517f40914c20a82e1dd77f634794b48b225
Heiko Scherrer <scherrer@openwms.org>
2016-05-08 22:57:19 +0200

    #77 Added entity base class to provide an application assigned id. Additionally started to implement a collection of save functions

---
+++ src/main/java/org/ameba/integration/SaveOperations.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2014-2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ameba.integration;
+
+import java.io.Serializable;
+
+/**
+ * A SaveOperations.
+ *
+ * @author <a href="mailto:scherrer@openwms.org">Heiko Scherrer</a>
+ * @version 1.0
+ * @since 1.6
+ */
+public interface SaveOperations<T extends TypedEntity<ID>, ID extends Serializable> {
+
+    /**
+     * Saves the state of the passed {@code entity} no matter whether the instance already exists or does not exist.
+     *
+     * @param entity The instance to save
+     * @return The saved instance
+     */
+    T save(T entity);
+}
---
+++ src/main/java/org/ameba/integration/jpa/ApplicationEntity.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2014-2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ameba.integration.jpa;
+
+import javax.persistence.Column;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+/**
+ * An ApplicationEntity adds a secondary key column that is an application assigned key that remains the same after database migrations.
+ *
+ * @author <a href="mailto:scherrer@openwms.org">Heiko Scherrer</a>
+ * @version 1.0
+ * @since 1.6
+ */
+@Table(uniqueConstraints = {@UniqueConstraint(columnNames = {ApplicationEntity.C_ID})})
+public class ApplicationEntity extends BaseEntity {
+
+    public static final String C_ID = "C_PID";
+    /**
+     * Technical or persisted key field, independent from the underlying database, assigned from application layer, remains the same over
+     * database migrations. An unique constraint or limitation to not-null is explicitly not defined here, because it is a matter of
+     * subclasses to defines those, if required.
+     */
+    @Column(name = ApplicationEntity.C_ID)
+    private Long id;
+
+    /**
+     * Get the persistent key.
+     *
+     * @return The id property
+     */
+    public Long getId() {
+        return id;
+    }
+}
