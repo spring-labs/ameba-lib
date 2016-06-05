39da1b019337ab073fe206d8e0a9c92cad7afcc7
Heiko Scherrer <scherrer@openwms.org>
2016-06-06 00:12:23 +0200

    improved poms set correct groupid

--- src/test/java/org/ameba/integration/jpa/IntegrationTestConfig.java
+++ src/test/java/org/ameba/integration/jpa/IntegrationTestConfig.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.ameba;
+package org.ameba.integration.jpa;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
