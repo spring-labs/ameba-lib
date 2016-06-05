1392c00ed173fb15994220e34d7948ea78f1e5a8
Heiko Scherrer <scherrer@openwms.org>
2016-06-05 15:28:06 -0100

    improved poms set correct groupid

--- src/main/java/org/ameba/integration/mongodb/BaseEntity.java
+++ src/main/java/org/ameba/integration/mongodb/BaseEntity.java
@@ -15,8 +15,12 @@
  */
 package org.ameba.integration.mongodb;
 
+import java.time.ZonedDateTime;
+
 import org.ameba.integration.TypedEntity;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.annotation.Version;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
@@ -33,17 +37,31 @@ public class BaseEntity implements TypedEntity<String> {
 
     /** Unique persistent key, assigned by Spring Data MongoDB or MongoDB itself. */
     @Id
-    @Field(BaseEntity.FIELD_ID)
+    @Field(FIELD_PK)
     private String pk;
     /** Field name to keep track of the current persisted identifier. */
-    public static final String FIELD_ID = "pk";
+    public static final String FIELD_PK = "_pk";
 
     /** Optimistic locking field, managed by Spring Data MongoDB. */
     @Version
-    @Field(BaseEntity.FIELD_OL_VERSION)
+    @Field(FIELD_OL_VERSION)
     private long ol;
     /** Field name of the optimistic locking field. */
-    public static final String FIELD_OL_VERSION = "ol";
+    public static final String FIELD_OL_VERSION = "_ol";
+
+    /** Timestamp when the document was inserted. */
+    @Field(FIELD_CREATED)
+    @CreatedDate
+    private ZonedDateTime createDt;
+    /** Field name of the timestamp when the document was inserted. */
+    public static final String FIELD_CREATED = "_created";
+
+    /** Timestamp when the document was updated the last time. */
+    @Field(FIELD_UPDATED)
+    @LastModifiedDate
+    private ZonedDateTime lastModifiedDt;
+    /** Field name of the timestamp when the document was updated the last time. */
+    public static final String FIELD_UPDATED = "_updated";
 
     /**
      * Checks whether this entity is a transient one or not.
@@ -70,5 +88,23 @@ public class BaseEntity implements TypedEntity<String> {
      */
     void setPk(String pk) {
         this.pk = pk;
+    }
+
+    /**
+     * Get the timestamp this document has been persisted the first time.
+     *
+     * @return Creation date
+     */
+    public ZonedDateTime getCreateDt() {
+        return createDt;
+    }
+
+    /**
+     * Get the timestamp this document has been modified the last time.
+     *
+     * @return Last modified date
+     */
+    public ZonedDateTime getLastModifiedDt() {
+        return lastModifiedDt;
     }
 }
