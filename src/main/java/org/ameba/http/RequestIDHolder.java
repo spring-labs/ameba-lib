8cc436b723a0128e97f8d4442ce64ef1de0e4927
Heiko Scherrer <scherrer@openwms.org>
2016-08-16 21:38:31 +0200

    #108 added requestID filter to create a requestID when not already exists and put that into a context holder.

---
+++ src/main/java/org/ameba/http/RequestIDHolder.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2014-2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ameba.http;
+
+/**
+ * A RequestHolder stores a requestID inside a thread local variable.
+ *
+ * @author <a href="mailto:scherrer@openwms.org">Heiko Scherrer</a>
+ * @since 1.7
+ */
+public class RequestIDHolder {
+
+    private static final InheritableThreadLocal<String> holder = new InheritableThreadLocal<>();
+
+    /** Private Constructor. */
+    private RequestIDHolder() {
+    }
+
+    /**
+     * Get the current request ID as String.
+     *
+     * @return RequestID as String
+     */
+    public static String getRequestID() {
+        return holder.get();
+    }
+
+    /**
+     * Set current request ID.
+     *
+     * @param requestID Request id to set
+     */
+    public static void setRequestID(String requestID) {
+        holder.set(requestID);
+    }
+
+    /**
+     * Checks whether a request ID is set in the current thread scope.
+     *
+     * @return {@link true} if so, otherwise {@literal false}
+     */
+    public static boolean hasRequestID() {
+        return holder.get() != null;
+    }
+
+    /**
+     * Cleanup thread local.
+     */
+    public static void destroy() {
+        holder.remove();
+    }
+}
