63ac4ae5123b131a772202594984be6c8d92afad
Heiko Scherrer <scherrer@openwms.org>
2023-05-31 23:18:41 +0200

    #171 Upgrade to SpringBoot 2.7.12 and Java 17; Removed unused build files; prepared for 3.0 release

--- src/test/java/org/ameba/integration/hibernate/HibernateSchemaBasedTenancyConfigurationTest.java
+++ src/test/java/org/ameba/integration/hibernate/HibernateSchemaBasedTenancyConfigurationTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2020 the original author or authors.
+ * Copyright 2015-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,28 +17,28 @@ package org.ameba.integration.hibernate;
 
 import org.ameba.app.BaseConfiguration;
 import org.ameba.tenancy.TenantHolder;
-import org.junit.AfterClass;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.cloud.sleuth.Tracer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Optional;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * A HibernateSchemaBasedTenancyConfigurationTest.
  *
  * @author Heiko Scherrer
  */
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @DataJpaTest(showSql = false, properties = "spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.H2Dialect")
 @ContextConfiguration(classes = HibernateITConfig.class)
 @ComponentScan(basePackageClasses = {HibernateSchemaBasedTenancyConfigurationTest.class, BaseConfiguration.class})
@@ -50,20 +50,18 @@ public class HibernateSchemaBasedTenancyConfigurationTest {
     @MockBean
     private Tracer tracer;
 
-    @AfterClass
+    @AfterAll
     public static void clearContext() {
         TenantHolder.destroy();
     }
 
-    public
-    @Test
-    void multipleSchemaTest() throws Exception {
+    @Test void multipleSchemaTest() throws Exception {
         /*
         we need to wrap all operations into a transactional service, because the connection gets pooled again
         after the tx commits. Unfortunately a DataJpaTest itself is a transactional test, therefore we cannot takeover
         control of transaction boundaries.
         */
-        // Lets create two empty schemas first
+        // Let's create two empty schemas first
         service.createSchema("SCHEMA_1");
         service.createSchema("SCHEMA_2");
 
