1712f877fbd804e8c9ba3f67b1773f098d47d17c
Heiko Scherrer <scherrer@openwms.org>
2024-12-05 15:20:12 +0100

    #190 Fix token parsing after jwt library upgrade

--- src/main/java/org/ameba/oauth2/DefaultTokenExtractor.java
+++ src/main/java/org/ameba/oauth2/DefaultTokenExtractor.java
@@ -17,17 +17,12 @@ package org.ameba.oauth2;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.Header;
-import io.jsonwebtoken.Jwt;
-import io.jsonwebtoken.JwtParser;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.impl.TextCodec;
+import io.jsonwebtoken.io.Decoders;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.util.Date;
 import java.util.List;
 
 import static java.lang.String.format;
@@ -41,6 +36,7 @@ import static java.lang.String.format;
 public class DefaultTokenExtractor implements TokenExtractor {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(DefaultTokenExtractor.class);
+    private final ObjectMapper om = new ObjectMapper();
     private final IssuerWhiteList<Issuer> whiteList;
     private final List<TokenParser> parsers;
 
@@ -55,7 +51,28 @@ public class DefaultTokenExtractor implements TokenExtractor {
     @Override
     public ExtractionResult canExtract(String token) {
         String[] parts = token.split("\\.");
-        return parts.length == 3 ? new ExtractionResult() : new ExtractionResult("Not a valid JWT");
+        return parts.length == 3 ? new ExtractionResult() : new ExtractionResult("Not a valid JWT, expected 3 parts");
+    }
+
+    private JsonNode parse(String token, ObjectMapper om) {
+        try {
+            return om.readValue(Decoders.BASE64URL.decode(token), JsonNode.class);
+        } catch (IOException e) {
+            LOGGER.error(e.getMessage(), e);
+            throw new InvalidTokenException("Part of token cannot be parsed");
+        }
+    }
+
+    private void ensureTokenNotExpired(JsonNode tokenPayload) {
+        long nowTime = new Date().getTime();
+        var exp = tokenPayload.get("exp").isNull() ? null : new Date(tokenPayload.get("exp").asLong() * 1000);
+        if (exp != null) {
+            var maxTime = nowTime - Issuer.DEFAULT_MAX_SKEW_SECONDS;
+            var max = new Date(maxTime);
+            if (max.after(exp)) {
+                throw new InvalidTokenException("Token has expired");
+            }
+        }
     }
 
     /**
@@ -64,29 +81,23 @@ public class DefaultTokenExtractor implements TokenExtractor {
     @Override
     public ExtractionResult extract(final String token) {
         // we do not trust the signature so first parse the token and check the issuer
-        String[] splitToken = token.split("\\.");
+        var splitToken = token.split("\\.");
         if (splitToken.length < 2) {
             throw new InvalidTokenException("Token is not a JWT");
         }
-        Jwt<Header, Claims> jwt;
-        JwtParser jwtParser = Jwts.parser().clockSkewSeconds(Issuer.DEFAULT_MAX_SKEW_SECONDS).build();
-        try {
-            jwt = jwtParser.parse(splitToken[0] + "." + splitToken[1] + ".").accept(Jwt.UNSECURED_CLAIMS);
-        } catch (ExpiredJwtException e) {
-            LOGGER.error(e.getMessage(), e);
-            throw new InvalidTokenException("Token has expired");
-        } catch (Exception e) {
-            LOGGER.error(e.getMessage(), e);
-            throw new InvalidTokenException("Token cannot be parsed");
-        }
+
+        var tokenHeader = parse(splitToken[0], om);
+        var tokenPayload = parse(splitToken[1], om);
+
+        ensureTokenNotExpired(tokenPayload);
 
         Issuer issuer;
-        if (jwt.getHeader().containsKey("kid")) {
+        if (tokenHeader.has("kid")) {
 
-            issuer = whiteList.getIssuer(jwt.getPayload().getIssuer(), "" + jwt.getHeader().get("kid"));
+            issuer = whiteList.getIssuer(tokenPayload.get("iss").asText(), tokenHeader.get("kid").asText());
         } else {
 
-            List<Issuer> issuers = whiteList.getIssuers(jwt.getBody().getIssuer());
+            var issuers = whiteList.getIssuers(tokenPayload.get("iss").asText());
             // Okay, the issuer seems to have multiple kids for the same issuer ID, so take the first one...
             issuer = issuers.isEmpty() ? null : issuers.getFirst();
         }
@@ -103,20 +114,14 @@ public class DefaultTokenExtractor implements TokenExtractor {
         }
 
         // Now check with Signature
-        ObjectMapper om = new ObjectMapper();
-        try {
-            JsonNode jsonNode = om.readValue(TextCodec.BASE64URL.decodeToString(token), JsonNode.class);
-            if (jsonNode.has("alg")) {
-                TokenParser parser = parsers.stream()
-                        .filter(p -> jsonNode.get("alg").asText().equals(p.supportAlgorithm()))
-                        .findFirst()
-                        .orElseThrow(() -> new InvalidTokenException(format("Algorithm [%s] not supported", jsonNode.get("alg"))));
-                return new ExtractionResult(parser.parse(token, issuer));
-            }
-            return new ExtractionResult("No alg claim defined in JWT header");
-        } catch (IOException e) {
-            LOGGER.error(e.getMessage(), e);
-            throw new InvalidTokenException("Token cannot be parsed into JSON");
+        if (tokenHeader.has("alg")) {
+            final var alg = tokenHeader.get("alg").asText();
+            var parser = parsers.stream()
+                    .filter(p -> alg.equals(p.supportAlgorithm()))
+                    .findFirst()
+                    .orElseThrow(() -> new InvalidTokenException(format("Algorithm [%s] not supported", alg)));
+            return new ExtractionResult(parser.parse(token, issuer));
         }
+        return new ExtractionResult("No alg claim defined in JWT header");
     }
 }
